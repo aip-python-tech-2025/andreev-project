6f58e10af1ca4f7164bda9b16ad1b787d260e0d2
Nikolay Andreev <bakasaru@list.ru>
2026-01-12 18:12:45 +0300

    feat: replace demo main script with example bot

--- main.py
+++ main.py
@@ -1,43 +1,20 @@
-from core.author import Author
-from core.book import Book
-from core.collection import Collection
-from core.genre import Genre
-from core.librarian import Librarian
-from core.user import User
+import os
+from dotenv import load_dotenv
+import telebot
 
-genre = Genre("Фантастика")
-author = Author('Джоан Роулинг')
-garry_1 = Book("Гарри Поттер и философский камень", "Высоко-высоко в горах...", [author], [genre])
-garry_2 = Book("Гарри Поттер и тайная комната", "Глубоко-глубоко под землёй...", [author], [genre])
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
-potter_books = Collection('Гарри Поттер')
-potter_books.add_book(garry_1)
-potter_books.add_book(garry_2)
-print(potter_books)
 
-user = User("Георгий", 1)
-user.add_book("Шерлок Холмс")
-user.return_book("Шерлок Холмс")
-user.add_book("Шерлок Холмс")
-user.list_of_unreturned_books()
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
 
-librarian = Librarian('Геннадий Петрович', [1, 2, 3, 4, 5], 2)
-librarian.add_book(garry_1)
-print(librarian)
 
-user.change_name(['Фамилия', 'Имя', 'Отчество'])
-user.change_name('ДУРАК')
-print(user.name)
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
 
-user.name += ' Петров'
-# user.name = user.name + 'Петров'
-print(user.name)
 
-user.name = 'Дурак'
-print(user.name)
-
-garry_1.add_user(user)
-print(user)
-
-for o in genre, author, user, librarian, garry_1, garry_2, potter_books:
-    print(o.__repr__())
+bot.infinity_polling()
--- requests-test.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = 'https://gutendex.com/books/'
-response = requests.get(url)
-data = response.json()
-print(data['results'][0]['title'])
