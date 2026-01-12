8eb0d7c0003ddc316f26b26948323bb377e8e3ea
Nikolay Andreev <bakasaru@list.ru>
2026-01-12 18:29:07 +0300

    feat: add simple keyboard

--- main.py
+++ main.py
@@ -7,9 +7,19 @@ TOKEN = os.getenv('TOKEN')
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	keyboard = telebot.types.ReplyKeyboardMarkup()
+	keyboard.add(
+		telebot.types.KeyboardButton('Взять книгу'),
+		telebot.types.KeyboardButton('Вернуть книгу')
+	)
+	keyboard.add(
+		telebot.types.KeyboardButton('Забронировать книгу'),
+		telebot.types.KeyboardButton('Найти книгу'),
+	)
+	bot.reply_to(message, 'Привет! Это бот библиотеки. Что ты хочешь сделать?', reply_markup=keyboard)
+
 
 
 @bot.message_handler(func=lambda m: True)
